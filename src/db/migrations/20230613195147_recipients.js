32a9238f2d763baff22ccee7be8d90449fcbcd41
professionaljared <misterjrd22@gmail.com>
2023-06-14 14:27:47 -0400

    making the migrations for the database tables

---
+++ src/db/migrations/20230613195147_recipients.js
@@ -0,0 +1,15 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = (knex) => knex.schema.createTable('recipients', (table) => {
+    table.increments('id');
+    table.integer('user_id').references("id").inTable('users');
+    table.string('requests');
+  });
+  
+  /**
+   * @param { import("knex").Knex } knex
+   * @returns { Promise<void> }
+   */
+  exports.down = (knex) => knex.schema.dropTable('recipients');
