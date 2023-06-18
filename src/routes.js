5bec7cb026f09c9df7c707caf270182158460cd6
gnatalie22 <gnatalie760@gmail.com>
2023-06-18 14:25:44 -0400

    mistake

--- src/routes.js
+++ src/routes.js
@@ -13,6 +13,7 @@ Router.post("/users", userController.create);
 Router.get("/users/:id", userController.show);
 Router.get("/users/:id/requests", requestController.showMe);
 
+Router.post("/requests/", requestController.create);
 Router.get("/requests/", requestController.list);
 Router.get("/requests/:id", requestController.show);
 
@@ -24,9 +25,6 @@ Router.patch("/users/:id", checkAuthentication, userController.update);
 Router.post("/login", userController.login);
 Router.delete("/logout", userController.logout);
 Router.get("/me", userController.showMe);
-
-Router.post('/request', requestController.create);
-
 
 Router.get("/logged-in-secret", checkAuthentication, (req, res) => {
   res.send({ msg: "The secret is: there is no secret." });
