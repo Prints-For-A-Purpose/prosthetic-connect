167b9879cc80a5136daf2cc1126fbd03c30c7e59
Julian Castro <jcas2tur@gmail.com>
2023-06-22 08:02:52 -0400

    added progress bar to Request.jsx

--- frontend/src/pages/Request.jsx
+++ frontend/src/pages/Request.jsx
@@ -13,6 +13,7 @@ import { getComments } from "../adapters/comments-adapter";
 import NewComment from "../components/NewComment";
 import CommentPagination from "../components/CommentPagination";
 import RequestInfo from "../components/RequestInfo";
+import ProgressBar from "../components/ProgressBar";
 
 export default function Request() {
   const { currentUser } = useContext(CurrentUserContext);
@@ -20,7 +21,23 @@ export default function Request() {
   const [username, setUsername] = useState(null);
   const [comments, setComments] = useState([]);
   const [errorText, setErrorText] = useState(null);
+  const [status, setStatus] = useState("");
   const { id } = useParams();
+
+  const progressStatus = {
+    Active: {
+      color: "#ff9800",
+      progress: 30,
+    },
+    In_progress: {
+      color: "#2196f3",
+      progress: 50,
+    },
+    Done: {
+      color: "#4caf50",
+      progress: 100,
+    },
+  };
 
   useEffect(() => {
     const loadRequest = async () => {
@@ -32,9 +49,12 @@ export default function Request() {
       const user = await getUser(data.user_id);
       const [{ username }] = user;
       setUsername(username);
+      setStatus(data.request_status);
     };
     loadRequest();
   }, [id]);
+
+  const { color, progress } = progressStatus[status] || progressStatus.Active;
 
   if (!request && !errorText && !username) return null;
   if (errorText) return <p>{errorText}</p>;
@@ -48,6 +68,7 @@ export default function Request() {
       </h2>
       <p>{request.timestamp}</p>
       <h3>Status: {request.request_status}</h3>
+      <ProgressBar color={color} progress={progress} />
       <RequestInfo request={request} currentUser={currentUser}></RequestInfo>
       <div>
         <h3>Comments</h3>
